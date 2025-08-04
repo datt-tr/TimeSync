c2e0a4c2c0268e5665cd565889f24b06dfcc9b98
Dat Tran <zrauchz@gmail.com>
2025-08-04 13:53:49 +0200

    refactor: testing purposes

--- TrackBot/Bots/EchoBot.cs
+++ TrackBot/Bots/EchoBot.cs
@@ -18,7 +18,7 @@ namespace EchoBot.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
+            var welcomeText = "Hello and welcome, my friend!";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
